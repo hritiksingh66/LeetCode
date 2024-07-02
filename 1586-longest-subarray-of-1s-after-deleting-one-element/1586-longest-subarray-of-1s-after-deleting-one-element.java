66d8afd0567c542bd791baf01aac0c2b9a8c4058
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2024-07-03 03:29:50 +0530

    Time: 3 ms (66.18%), Space: 57.6 MB (5.83%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
@@ -4,20 +4,18 @@ class Solution {
         int n = nums.length;
 
         int i = 0;
+        int j = 0;
+        int last_zero_idx = -1;
 
-        int zerosCount = 0;
         int result = 0;
 
-        for(int j = 0 ; j < n ; j++){
-
-            if(nums[j]==0) zerosCount++;
-
-            while(zerosCount > 1){
-                if(nums[i]==0) zerosCount--;
-                i++;
+        while(j < n ){
+            if(nums[j]==0){ 
+                i = last_zero_idx + 1;
+                last_zero_idx = j;
             }
-
             result = Math.max(result , j-i);
+            j++;
         }
 
         return result;
