08b86e69a1ed28a6723aa6e51d19c608f77d3c6b
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2024-07-02 18:56:41 +0530

    Time: 11 ms (86.81%), Space: 44.2 MB (85.77%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,6 +1,13 @@
-// STORY POINTS 
+/* STORY POINTS 
+1. i = 0 , j = 0 --> for two ends of window
 
+2. keep counting vowel if s[j] is 'a' , 'e','i','o' or 'u'
 
+3. when you get window of size k i.e, (j-i+1)==k
+   3.1 Update maxV
+   3.2 shift window Right side i.e, i++ but also update vowel count if(s[i] was a vowel)then count--;
+   
+*/
 
 class Solution {
     boolean isVowel(char ch){
