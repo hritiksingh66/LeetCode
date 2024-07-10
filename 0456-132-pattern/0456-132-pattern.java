db733c76b173a448594bdbdb12ebf4fc220a46de
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2024-07-10 18:15:31 +0530

    Time: 49 ms (19.07%), Space: 76.2 MB (22.47%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+
+        int num3 = Integer.MIN_VALUE;
+
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = n-1 ; i >= 0 ; i--){
+            if(nums[i] < num3){
+                return true;
+            }
+
+            while(!st.isEmpty() && st.peek() < nums[i]){
+                num3 = st.peek();
+                st.pop();
+            }
+
+            st.push(nums[i]);
+        }
+        
+        return false;
+    }
+}
