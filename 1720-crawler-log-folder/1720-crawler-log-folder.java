f7a2b53c234976c094148a220f5a811b6740af30
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2024-07-10 16:21:48 +0530

    Time: 1 ms (99.42%), Space: 41.9 MB (51.62%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.java
+++ 1720-crawler-log-folder/1720-crawler-log-folder.java
@@ -3,7 +3,6 @@ class Solution {
         int depth = 0;
 
         for(String log : logs){
-            System.out.println(log.equals("../"));
             if(log.equals("../")){
                 depth = Math.max(0,depth-1);
             }else if(log.equals("./")){
