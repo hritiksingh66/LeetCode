91cd782fa006183b32d6bef0db3f2829c9e4226a
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2024-08-14 17:58:19 +0530

    Time: 355 ms (18.2%), Space: 44.7 MB (21.77%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -1,17 +1,26 @@
-import java.util.*;
 class Solution {
     public int smallestDistancePair(int[] nums, int k) {
-    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        int largest = Integer.MIN_VALUE;
+        for (int num : nums) {
+            largest = Math.max(largest, num);
+        }
+        largest += 1;
+        int[] temp = new int[largest];
 
-        for(int i = 0 ; i < nums.length -1; i++){
-            for(int j = i+1 ; j < nums.length ; j++){
-                maxHeap.offer(Math.abs(nums[j]-nums[i]));
-                if(maxHeap.size() > k){
-                    maxHeap.poll();
-                }
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                temp[Math.abs(nums[i] - nums[j])]++;
             }
         }
 
-        return maxHeap.peek();
+        for (int i = 0; i <= largest; i++) {
+            k -= temp[i];
+            if (k <= 0) {
+                return i;
+            }
+        }
+
+        return 0;
+
     }
 }
