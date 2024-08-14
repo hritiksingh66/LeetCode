52ace39ddc5c5c70a46b7339f51ba26d59d9f5e5
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2024-08-14 21:59:08 +0530

    Time: 8 ms (81.12%), Space: 44.6 MB (21.77%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -1,26 +1,40 @@
 class Solution {
+
+    // Find count of pairs having distance <= D
+    private int slidingWindowCount(int[] nums, int D) {
+        int count = 0;
+        int n = nums.length;
+        int i = 0;
+        int j = 1;
+
+        while (j < n) {
+            while (nums[j] - nums[i] > D) {
+                i++;
+            }
+            count += j - i;
+            j++;
+        }
+        return count;
+    }
+
     public int smallestDistancePair(int[] nums, int k) {
-        int largest = Integer.MIN_VALUE;
-        for (int num : nums) {
-            largest = Math.max(largest, num);
-        }
-        largest += 1;
-        int[] temp = new int[largest];
+        Arrays.sort(nums);
+        int n = nums.length;
+        int l = 0;
+        int r = nums[n - 1] - nums[0];
+        int result = 0;
 
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                temp[Math.abs(nums[i] - nums[j])]++;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            int count = slidingWindowCount(nums, mid);
+
+            if (count < k) {
+                l = mid + 1;
+            } else {
+                result = mid; // Store the mid as a potential result
+                r = mid - 1;
             }
         }
-
-        for (int i = 0; i <= largest; i++) {
-            k -= temp[i];
-            if (k <= 0) {
-                return i;
-            }
-        }
-
-        return 0;
-
+        return result;
     }
 }
