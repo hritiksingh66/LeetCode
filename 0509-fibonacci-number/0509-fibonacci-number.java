a7884d3aa8b1acba1b3d2c5e34c02a449eca05c3
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2024-08-18 01:03:18 +0530

    Time: 0 ms (100%), Space: 40 MB (89.51%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,22 +1,19 @@
 class Solution {
-    public int solve(int n , int[] dp){
-        if( n <= 1){
-            return n;
-        }
-
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        return dp[n] = solve(n-1 , dp) + solve(n-2 , dp);
-    }
-
     public int fib(int n) {
-        if(n <= 1)  // Lowest possible value of param
+        if(n <= 1 ){
             return n;
+        }
 
         int[] dp = new int[n+1];
-        Arrays.fill(dp , -1);    
+        Arrays.fill(dp , -1);
 
-        return solve(n , dp);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2 ; i <= n ; i++){
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+
+        return dp[n];
     }
 }
