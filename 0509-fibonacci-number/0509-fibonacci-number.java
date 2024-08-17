4d0b2ebd560a8b818e95a2ce95c151c38235fc6b
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2024-08-18 01:06:28 +0530

    Time: 0 ms (100%), Space: 40.5 MB (24.13%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,19 +1,21 @@
+// T.C = O(n);
+// S.C = O(n);
 class Solution {
     public int fib(int n) {
         if(n <= 1 ){
             return n;
         }
+        int a = 0;
+        int b = 1;
+        int c = 0;
 
-        int[] dp = new int[n+1];
-        Arrays.fill(dp , -1);
-
-        dp[0] = 0;
-        dp[1] = 1;
 
         for(int i = 2 ; i <= n ; i++){
-            dp[i] = dp[i-2] + dp[i-1];
+            c = a + b;
+            a = b;
+            b = c;
         }
 
-        return dp[n];
+        return c;
     }
 }
