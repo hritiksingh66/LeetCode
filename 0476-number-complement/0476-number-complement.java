10f17d0956118286733981912cf1bb0dabe21515
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2024-08-22 10:26:04 +0530

    Time: 0 ms (100%), Space: 40.2 MB (67.03%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,23 +1,9 @@
 class Solution {
-    public static String toBinaryString(int number) {
-        if (number == 0) {
-            return "0";
-        }
+    public int findComplement(int num) {
+        // Create a mask with all bits set to 1 that has the same length as num
+        int mask = (Integer.highestOneBit(num) << 1) - 1;
         
-        StringBuilder binaryString = new StringBuilder();
-        while (number > 0) {
-            binaryString.insert(0, number % 2);
-            number = number / 2;
-        }
-        return binaryString.toString();
-    }
-    public int findComplement(int num){
-        String s = toBinaryString(num);
-
-        int n = s.length();
-        int temp = (int)Math.pow(2,n) - 1;
-
-
-        return temp - num;
+        // XOR the number with the mask to get the complement
+        return num ^ mask;
     }
 }
