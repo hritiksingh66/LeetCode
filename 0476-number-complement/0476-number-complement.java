491eb0d0db2bcd19bf9aee7b606b1a1d97e28d34
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2024-08-22 10:26:15 +0530

    Time: 0 ms (100%), Space: 39.9 MB (95.53%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findComplement(int num) {
         // Create a mask with all bits set to 1 that has the same length as num
-        int mask = (Integer.highestOneBit(num) << 1) - 1;
+        int mask = (Integer.highestOneBit(num) * 2) - 1;
         
         // XOR the number with the mask to get the complement
         return num ^ mask;
