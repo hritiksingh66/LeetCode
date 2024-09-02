dc49b13fa4707124ce63626280342d64f730748f
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2024-09-03 00:10:48 +0530

    Time: 1869 ms (15.86%), Space: 60.2 MB (31.16%) - LeetHub

---
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int chalkReplacer(int[] chalk, int k){
+        while(true){
+            for(int i = 0;i < chalk.length ; i++){
+
+                if(k < chalk[i]){
+                    return i;
+                }
+                k -= chalk[i];
+            }
+        }
+    }
+}
