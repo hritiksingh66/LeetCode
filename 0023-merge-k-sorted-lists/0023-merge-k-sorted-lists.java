b3d9304a9a77efc0edba2f9018df800704cce25e
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2024-09-13 09:49:36 +0530

    Time: 4 ms (73.81%), Space: 44.4 MB (40.37%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,38 +9,39 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoList(ListNode l1,ListNode l2){
-        if(l2==null) return l1;
-        if(l1==null) return l2;
-        if(l1.val<=l2.val){
-            l1.next = mergeTwoList(l1.next,l2);
-            return l1;
-        }else{
-            l2.next = mergeTwoList(l1,l2.next);
-            return l2;
-        }
-    }
-    public ListNode partitionAndMerge(int start,int end,ListNode[] lists){
-        if(start>end){
-            return null;
-        }
-        
-        if(start==end){
-            return lists[start];
-        }
-        
-        int mid = start+(end-start)/2;
-        ListNode l1 = partitionAndMerge(start,mid,lists);
-        ListNode l2 = partitionAndMerge(mid+1,end,lists);
-        
-        return mergeTwoList(l1,l2);
-    }
-    
     public ListNode mergeKLists(ListNode[] lists){
-        int k = lists.length;
-        
-        if(k==0) return null;
-        
-        return partitionAndMerge(0,k-1,lists);
-    } 
+
+        PriorityQueue<ListNode> priorityQueue = new PriorityQueue<>(new Comparator<ListNode>(){
+            @Override
+            public int compare(ListNode n1, ListNode n2) {
+                return Integer.compare(n1.val, n2.val); 
+            }
+        });
+
+
+        for(ListNode li : lists){
+
+            if(li != null){
+                priorityQueue.add(li);
+            }
+        }
+
+        ListNode dummy = new ListNode();
+
+        ListNode curr = dummy;
+
+        while(!priorityQueue.isEmpty()){
+            ListNode temp = priorityQueue.poll();
+            curr.next = new ListNode(temp.val);
+            curr = curr.next;
+
+           
+            if(temp.next != null){
+                priorityQueue.add(temp.next);
+            }
+
+        }
+
+        return dummy.next;
+    }
 }
