9ce9d6683bd5c24b406188887d4fada040102ca3
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2024-09-15 23:08:31 +0530

    Time: 55 ms (46.62%), Space: 45.1 MB (83.99%) - LeetHub

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.java
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -1,38 +1,32 @@
 class Solution {
     public int findTheLongestSubstring(String s){
 
-        Map<String,Integer> mp = new HashMap<>();
+        Map<Integer,Integer> mp = new HashMap<>();
 
         int result = 0;
-        int []state = new int[5]; 
-        String currState = "00000";
+        int mask = 0;
 
-        mp.put(currState,-1);
+        mp.put(mask,-1);
 
         for(int i = 0 ; i < s.length() ; i++){
 
             if(s.charAt(i)=='a'){
-                state[0] = (state[0] ^ 1);
+                mask = mask ^ (1<<0);
             }else if(s.charAt(i)=='e'){
-                state[1] = (state[1] ^ 1);
+                mask = mask ^ (1<<1);
             }else if(s.charAt(i)=='i'){
-                state[2] = (state[2] ^ 1);
+                mask = mask ^ (1<<2);
             }else if(s.charAt(i)=='o'){
-                state[3] = (state[3] ^ 1);
+                mask = mask ^ (1<<3);
             }else if(s.charAt(i)=='u'){
-                state[4] = (state[4] ^ 1);
+                mask = mask ^ (1<<4);
             }
 
-            currState = "";
 
-            for(int j = 0 ; j < 5; j++){
-                currState += Integer.toString(state[j]);
-            }
-
-            if(mp.containsKey(currState)){
-                result = Math.max(result,i-mp.get(currState));
+            if(mp.containsKey(mask)){
+                result = Math.max(result,i-mp.get(mask));
             }else{
-                mp.put(currState,i);
+                mp.put(mask,i);
             }
         }
 
