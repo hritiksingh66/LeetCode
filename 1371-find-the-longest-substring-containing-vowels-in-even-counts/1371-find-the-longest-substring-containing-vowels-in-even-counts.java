3dd1f7777c875b1227642132371321d01ef79521
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2024-09-15 23:08:22 +0530

    Time: 461 ms (5.34%), Space: 49.2 MB (23.49%) - LeetHub

--- 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.java
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -12,15 +12,15 @@ class Solution {
         for(int i = 0 ; i < s.length() ; i++){
 
             if(s.charAt(i)=='a'){
-                state[0] = (state[0] + 1)%2;
+                state[0] = (state[0] ^ 1);
             }else if(s.charAt(i)=='e'){
-                state[1] = (state[1] + 1)%2;
+                state[1] = (state[1] ^ 1);
             }else if(s.charAt(i)=='i'){
-                state[2] = (state[2] + 1)%2;
+                state[2] = (state[2] ^ 1);
             }else if(s.charAt(i)=='o'){
-                state[3] = (state[3] + 1)%2;
+                state[3] = (state[3] ^ 1);
             }else if(s.charAt(i)=='u'){
-                state[4] = (state[4] + 1) % 2;
+                state[4] = (state[4] ^ 1);
             }
 
             currState = "";
