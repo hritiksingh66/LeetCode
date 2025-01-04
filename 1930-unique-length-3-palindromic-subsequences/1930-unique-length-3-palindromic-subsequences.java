3b74c9e0bc47d45a6445fefb8f3a50fb23b1ccd9
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-01-04 21:19:52 +0530

    Time: 28 ms (92.67%), Space: 45.6 MB (39.53%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -1,48 +1,28 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        int n = s.length();
-        int pre[][] = new int[n][26];
-        int suff[][] = new int[n][26];
-
-        pre[0][s.charAt(0)-'a']++;
-
-        for(int i=1; i<n; i++){
-            char ch = s.charAt(i);
-          //  pre[i] = pre[i-1];
-            for(int j=0; j<26; j++){
-                pre[i][j] = pre[i-1][j];
-            }
-            pre[i][ch-'a'] = pre[i-1][ch-'a']+1;
+        int[] left = new int[26];
+        int[] right = new int[26];
+        int rs = 0 ; 
+        for (char c : s.toCharArray()) {
+            ++right[c - 'a'];
         }
-
-        suff[n-1][s.charAt(n-1)-'a']++;
-
-        for(int i=n-2; i>=0; i--){
-            for(int j=0; j<26; j++){
-                suff[i][j] = suff[i+1][j];
-            }
-            char ch = s.charAt(i);
-            suff[i][ch-'a'] = suff[i+1][ch-'a']+1;
-        }
-
-        HashSet<String> hp = new HashSet<>();
-        for(int i=1; i<n-1; i++){
-            boolean leftSame = false;
-            boolean rightSame = false;
-            char ch = s.charAt(i);
-
-            if(pre[i-1][ch-'a']>0) leftSame = true;
-            if(suff[i+1][ch-'a']>0) rightSame = true;
-
-            if(leftSame && rightSame) hp.add(""+ch+ch+ch);
-
-            for(int j=0; j<26; j++){
-                if(pre[i-1][j]>0 && suff[i+1][j]>0){
-                    char x = (char)(j+'a');
-                    hp.add(""+x+ch+x);
+        int[] hash = new int[26] ; 
+        for (char c : s.toCharArray()) {
+            int in = c - 'a' ;
+            --right[in];
+            for (int j = 0; j < 26; ++j) {
+                if ( right[j] > 0 && left[j] > 0 ) {
+                    hash[j] |= ( 1 << in ) ; 
                 }
             }
+            ++left[in];
         }
-        return hp.size();
+        for( int i : hash ){
+            while( i > 0 ){
+                rs += ( i & 1 ) ; 
+                i >>= 1 ;
+            }
+        }
+        return rs ; 
     }
 }
