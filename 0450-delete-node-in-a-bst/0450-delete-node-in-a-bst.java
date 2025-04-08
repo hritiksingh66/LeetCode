05fba1a831fd514049cab8992c47ef89812e17e9
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-04-08 15:39:36 +0530

    Time: 0 ms (100%), Space: 46.1 MB (61.66%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -15,41 +15,39 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root == null ) {
-            return null;
-        }
-        if(root.val<key){
-            root.right = deleteNode(root.right,key);
-        }
-        else if(root.val>key) {
-            root.left = deleteNode(root.left,key);
-        }
-        else{
-            //case 1: leaf node
-            if(root.left == null && root.right == null ) {
-                return null;
-            }
-            //case 2 : one child
-            if(root.left ==  null){
-                return root.right;
-            }
-            else if(root.right ==  null ) {
-                return root.left;
-            }
-            //case 3 : two children
-            TreeNode IS = findInOrderSuccessor(root.right);
-            root.val = IS.val;
 
-            
-            root.right = deleteNode(root.right,IS.val);
-        }
-        return root;
+        return delete(root, key);
 
     }
-    private TreeNode findInOrderSuccessor(TreeNode root){
-        while(root.left != null){
-            root = root.left;
+
+    public TreeNode delete(TreeNode root, int key) {
+        if (root == null) {
+            return null;
         }
+        if (root.val < key) {
+            root.right = delete(root.right, key);
+        } else if (root.val > key) {
+            root.left = delete(root.left, key);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            } else {
+                int max = max(root.left);
+                root.left = delete(root.left, max);
+                root.val = max;
+            }
+        }
+
         return root;
+    }
+
+    private int max(TreeNode node) {
+        if (node == null) {
+            return Integer.MIN_VALUE;
+        }
+        int right = max(node.right);
+        return Math.max(node.val, right);
     }
 }
