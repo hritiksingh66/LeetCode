a11be0585d1099b55603ea0bd5233954537f3aad
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-04-20 19:01:15 +0530

    Time: 4 ms (99.2%), Space: 45.4 MB (16.76%) - LeetHub

--- 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.java
+++ 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.java
@@ -1,20 +1,26 @@
 class Solution {
     public int longestAlternatingSubarray(int[] nums, int threshold) {
-        int l = 100000 , i = 0;
+        int l = -1;  // Start index of current valid subarray
         int ans = 0;
 
-        while( i < nums.length){
-            if(nums[i]>threshold || (i>0 && nums[i]%2==nums[i-1]%2)){
-                ans = Math.max(ans,i-l);
-                l=100000;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > threshold || (i > 0 && l != -1 && nums[i] % 2 == nums[i - 1] % 2)) {
+                if (l != -1) {
+                    ans = Math.max(ans, i - l);
+                }
+                l = -1;
             }
 
-            if(l==100000 &&  nums[i]<=threshold && nums[i]%2==0){
-                l=i;
+            if (l == -1 && nums[i] % 2 == 0 && nums[i] <= threshold) {
+                l = i;
             }
-            i++;
         }
-        ans = Math.max(ans,i-l);
+
+        // Final check
+        if (l != -1) {
+            ans = Math.max(ans, nums.length - l);
+        }
+
         return ans;
     }
 }
