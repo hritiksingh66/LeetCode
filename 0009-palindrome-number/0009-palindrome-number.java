9027a8b55b0907fd92eea884a73f0cfc4629c2a7
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-04-26 00:59:30 +0530

    Time: 4 ms (100%), Space: 44.3 MB (26.33%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,23 +1,16 @@
-class Solution 
-{
-    public boolean isPalindrome(int x) 
-    {
-        int temp=x;
-        int rev=0;
-        while(x>0)
-        {
-            int digit=x%10;
-            rev=(rev*10)+digit;
-            x=x/10;
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp = x;
+
+        int formedNo = 0;
+
+        // x = Math.abs(x);
+
+        while (x > 0) {
+            formedNo = (formedNo * 10) + x % 10;
+            x = x / 10;
         }
-        if(rev==temp)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-        
+
+        return temp == formedNo;
     }
 }
