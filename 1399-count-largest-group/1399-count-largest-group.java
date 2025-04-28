0b77c32a4279c5d9f4949ee9f0fd699728ee98ec
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-04-28 15:51:36 +0530

    Time: 3 ms (98.09%), Space: 41 MB (59.24%) - LeetHub

--- 1399-count-largest-group/1399-count-largest-group.java
+++ 1399-count-largest-group/1399-count-largest-group.java
@@ -1,31 +1,29 @@
 class Solution {
-    public int findDigitSum(int num){
-        int digitSum = 0;
-        while(num > 0){
-            digitSum += num % 10;
-            num = num/10;
-        }
-
-        return digitSum;
-    }
-    public int countLargestGroup(int n){
-        Map<Integer,Integer> map = new HashMap<>();
-
-        int maxSize = 0;
+    public int countLargestGroup(int n) {
+        int[] arr = new int[n + 1];
+        int max = 0;
         int count = 0;
 
-        for(int num = 1 ; num <= n; num++){
-            int digitSum = findDigitSum(num);
-            map.put(digitSum , map.getOrDefault(digitSum,0)+1);
+        for (int num = 1; num <= n; num++) {
 
-            if(map.get(digitSum) > maxSize){
-                maxSize = map.get(digitSum);
-                count = 1;
-            }else if(map.get(digitSum) == maxSize){
-                count++;
+            int digit = num;
+            int digitSum = 0;
+
+            while (digit > 0) {
+                digitSum += digit % 10;
+                digit /= 10;
             }
+
+            arr[digitSum]++;
+
+            max = Math.max(max, arr[digitSum]);
+        }
+
+        for(int i=1;i<arr.length;i++){
+            if(arr[i] == max) count++;
         }
 
         return count;
+
     }
 }
