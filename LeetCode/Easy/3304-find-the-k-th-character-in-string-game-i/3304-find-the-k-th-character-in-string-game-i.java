31dcb3797e62cd91be3ec79266efc3d197260aac
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-07-04 23:59:42 +0530

    3304-find-the-k-th-character-in-string-game-i Easy .java 07-04-2025 0 ms (100%)

--- LeetCode/Easy/3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.java
+++ LeetCode/Easy/3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.java
@@ -1,16 +1,5 @@
 class Solution {
     public char kthCharacter(int k) {
-        String str = "a";
-        while (str.length() < k) {
-            int n = str.length();
-            for (int i = 0; i < n; i++) {
-                char ch = str.charAt(i);
-                char addingChar = (ch == 'z') ? 'a' : (char) (ch + 1);
-                str += addingChar;
-            }
-        }
-
-        return str.charAt(k-1);
-
+        return (char)('a' + Integer.bitCount(k - 1));
     }
 }
