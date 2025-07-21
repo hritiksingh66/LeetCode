2af2a8ace85c382071f54b150a543b08dd87f42d
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-07-21 23:57:57 +0530

    1319-number-of-operations-to-make-network-connected Medium .java 07-21-2025 4 ms (90.89%)

---
+++ LeetCode/Medium/1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,57 @@
+class Solution {
+    int[] parent;
+    int[] rank; 
+    public int makeConnected(int n, int[][] connections){
+        int edges = connections.length;
+        if( edges < (n-1)) return -1;
+
+        parent = new int[n];
+
+        for(int i = 0 ; i <n;i++){
+            parent[i] = i;
+        }
+
+        rank = new int[n];
+        // n --> starting me total n components the
+
+        for(int[] ed : connections){
+            int x = ed[0];
+            int y = ed[1];
+
+            if(find(x) != find(y)){
+                union(x,y);
+                n -= 1; // union karne pe components (ilaako) ka sankhya 1 se kam ho jayega na ji 
+            }else{
+                continue;
+            }
+        }
+
+        return n - 1;
+    }
+
+
+    int find(int x){
+        if(x == parent[x]){
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    void union(int x , int y){
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if(x_parent == y_parent){
+            return;
+        }
+        if(rank[x_parent] > rank[y_parent]){
+            parent[y_parent] = x_parent;
+        }else if(rank[x_parent] < rank[y_parent]){
+            parent[x_parent] = y_parent;
+        }else{
+            parent[x_parent] = y_parent;
+            rank[y_parent] += 1; 
+        }
+    }
+}
