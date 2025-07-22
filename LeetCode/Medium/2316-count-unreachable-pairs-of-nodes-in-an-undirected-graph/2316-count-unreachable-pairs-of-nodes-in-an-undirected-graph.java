759402537810b52797ac927195266957f6b3d6e7
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-07-22 09:31:49 +0530

    2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph Medium .java 07-22-2025 53 ms (25.75%)

--- LeetCode/Medium/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
+++ LeetCode/Medium/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
@@ -1,66 +1,31 @@
 class Solution {
-    int[] parent;
-    int[] rank;
-
-    int find(int x){
-        if(x == parent[x]){
-            return x;
+    int dfs(ArrayList<ArrayList<Integer>> l ,boolean[] vis,int u){
+        vis[u] = true;
+        int cnt = 1;
+        for(int ne:l.get(u)){
+            if(!vis[ne]) cnt += dfs(l,vis,ne);
         }
-
-        return parent[x] = find(parent[x]);
-    }
-
-    void union(int x, int y){
-        int x_parent = find(x);
-        int y_parent = find(y);
-
-        if(x_parent == y_parent){
-            return;
-        }
-
-        if(rank[x_parent] > rank[y_parent]){
-            parent[x_parent] = y_parent;
-        }else if(rank[x_parent] < rank[y_parent]){
-            parent[y_parent] = x_parent;
-        }else{
-            parent[y_parent] = x_parent;
-            rank[x_parent] += 1;
-        }
+        return cnt;
     }
 
     public long countPairs(int n, int[][] edges){
-        parent = new int[n];
-        for(int i = 0 ; i < n; i++){
-            parent[i] = i;
+        ArrayList<ArrayList<Integer>> l = new ArrayList<>();
+        for(int i=0;i<n;i++) l.add(new ArrayList<>());
+        for(int[] e:edges){
+            int u = e[0];
+            int v = e[1];
+            l.get(u).add(v);
+            l.get(v).add(u);
         }
-
-        rank = new int[n];
-
-
-        for(int[] edge: edges){
-            int x = edge[0];
-            int y = edge[1];
-
-            if(find(x) != find(y)){
-                union(x,y);
-            }else{
-                continue;
-            }
-        }
-
-        Map<Integer,Integer> mp = new HashMap<>();
-
-        for(int i = 0 ; i < n;i++){
-            mp.put(find(i),mp.getOrDefault(find(i),0)+1);
-        }
-
+        boolean[] vis = new boolean[n];
         long res = 0;
-
         long remaining = n;
-
-        for(int size : mp.values()){
-            res += (long)size*(remaining - size);
-            remaining -= size;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                int size = dfs(l,vis,i);
+                res += (long)size*(remaining - size);
+                remaining -= size;
+            }
         }
         return res;
     }
