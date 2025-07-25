2724ec490818e5cf4d37d189b146968381494170
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-07-25 19:42:03 +0530

    2322-minimum-score-after-removals-on-a-tree Hard .java 07-25-2025 39 ms (68.75%)

--- LeetCode/Hard/2322-minimum-score-after-removals-on-a-tree/2322-minimum-score-after-removals-on-a-tree.java
+++ LeetCode/Hard/2322-minimum-score-after-removals-on-a-tree/2322-minimum-score-after-removals-on-a-tree.java
@@ -1,54 +1,80 @@
 class Solution {
+    int[] inTime;
+    int[] outTime;
+    int[] subtreeXOR;
+    List<List<Integer>> adj;
+    int timer = 0; 
+
     public int minimumScore(int[] nums, int[][] edges) {
-        int n = nums.length;
-        xor = new int[n];
-        tree = new ArrayList[n];
-        for(int i=0;i<n;i++)tree[i] = new ArrayList<>();
-        for(int[] e : edges){
-            tree[e[0]].add(e[1]);
-            tree[e[1]].add(e[0]);
+        int V = nums.length;
 
+        adj = new ArrayList<>();
+
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
         }
-        dfs1(0,-1,nums);
-        // System.out.println(Arrays.toString(xor));
-        dfs2(0,new HashSet<>(),-1);
-        return ans;
 
-    }  
-    int ans = (int)1e9;
-    List<Integer> [] tree;  
-    int[] xor ;   
-    private  void dfs1(int x, int p,int[] nums){
-        xor[x] = nums[x];
-        for(int y : tree[x]){
-            if(y!=p){
-                dfs1(y,x,nums);
-                xor[x] ^= xor[y];
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        subtreeXOR = new int[V];
+        inTime = new int[V];
+        outTime = new int[V];
+
+        // Call DFS with root 0 and parent -1
+        DFS(0, -1, nums);
+
+        int res = Integer.MAX_VALUE;
+
+        for (int u = 1; u < V - 1; u++) {
+            for (int v = u + 1; v < V; v++) {
+                int xor1, xor2, xor3;
+
+                if (isAncestor(u, v)) {
+                    xor1 = subtreeXOR[v];
+                    xor2 = subtreeXOR[u] ^ subtreeXOR[v];
+                } else if (isAncestor(v, u)) {
+                    xor1 = subtreeXOR[u];
+                    xor2 = subtreeXOR[v] ^ subtreeXOR[u];
+                } else {
+                    xor1 = subtreeXOR[u];
+                    xor2 = subtreeXOR[v];
+                }
+
+                xor3 = subtreeXOR[0] ^ xor1 ^ xor2;
+                res = Math.min(res, getScore(xor1, xor2, xor3));
             }
-            
         }
+
+        return res;
     }
-    private void dfs2(int x, Set<Integer> set, int p){
-        set.add(x);
-        dfs3(0,-1,set,x);
-        for(int y : tree[x])
-            if(y!=p)dfs2(y,set,x);
-        set.remove(x);
+
+    public int getScore(int a, int b, int c) {
+        int largest = Math.max(a, Math.max(b, c));
+        int smallest = Math.min(a, Math.min(b, c));
+        return largest - smallest;
     }
-    private void dfs3(int x, int p, Set<Integer> ancestor,int a){
-        if(x == a)return;
-        int b,c;
-        if(ancestor.contains(x)){
-             b = xor[x] ^ xor[a];
-             c = xor[0] ^ xor[x];
-        }else{
-            b = xor[x];
-            c = xor[0] ^ xor[a] ^ b;
+
+    public boolean isAncestor(int u, int v) {
+        return inTime[v] >= inTime[u] && outTime[u] >= outTime[v];
+    }
+
+    public void DFS(int node, int parent, int[] nums) {
+        inTime[node] = timer++;
+        subtreeXOR[node] = nums[node];
+
+        for (int nbr : adj.get(node)) {
+            if (nbr != parent) {
+                DFS(nbr, node, nums);
+                subtreeXOR[node] ^= subtreeXOR[nbr];
+            }
         }
-        int curr = Math.max(xor[a],Math.max(b,c)) - Math.min(xor[a],Math.min(b,c));
-        if(x!=0)ans = Math.min(ans,curr);
-        // System.out.println(x+" "+a+" "+xor[a]+" "+b+" "+c);
-        for(int y : tree[x])
-            if(y!=p)dfs3(y,x,ancestor,a);
+
+        outTime[node] = timer++;
     }
 }
