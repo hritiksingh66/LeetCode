5c753e38a54f573b99ef6d392569d60d3cd044dd
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-07-28 12:03:40 +0530

    2044-count-number-of-maximum-bitwise-or-subsets Medium .java 07-28-2025 43 ms (28.57%)

--- LeetCode/Medium/2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
+++ LeetCode/Medium/2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
@@ -1,24 +1,27 @@
 class Solution {
-    int count = 0;
     int targetOR;
+    int[][] dp;
     public int countMaxOrSubsets(int[] nums) {
         targetOR = 0;
         for (int num : nums) {
             targetOR |= num;
         }
-        backtrack(nums, 0, 0);
+        dp = new int[nums.length][targetOR+1];
+        for(int[] d:dp) Arrays.fill(d,-1);
+        return backtrack(nums, 0, 0);
 
-        return count;
     }
 
-    private void backtrack(int[] nums, int index, int currOR) {
+    private int backtrack(int[] nums, int index, int currOR) {
         if (index == nums.length) {
             if (currOR == targetOR) {
-                count++;
+                return 1;
             }
-            return;
+            return 0;
         }
-        backtrack(nums, index + 1, currOR | nums[index]);
-        backtrack(nums, index + 1, currOR);
+        if(dp[index][currOR]!=-1) return dp[index][currOR];
+        int t = backtrack(nums, index + 1, currOR | nums[index]);
+        int not = backtrack(nums, index + 1, currOR);
+        return dp[index][currOR] = t+not;
     }
 }
