9726cbbca0e0ecee0433cda2fe670b0eabc2f9ce
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-08-02 15:26:56 +0530

    2163-minimum-difference-in-sums-after-removal-of-elements Hard .java 08-02-2025 186 ms (15.36%)

--- LeetCode/Hard/2163-minimum-difference-in-sums-after-removal-of-elements/2163-minimum-difference-in-sums-after-removal-of-elements.java
+++ LeetCode/Hard/2163-minimum-difference-in-sums-after-removal-of-elements/2163-minimum-difference-in-sums-after-removal-of-elements.java
@@ -3,44 +3,39 @@ class Solution {
         int N = nums.length;
         int n = N / 3;
 
-        // ---------- Left Min Sum ----------
         long[] leftMinSum = new long[N];
-        PriorityQueue<Integer> leftPq = new PriorityQueue<>(Collections.reverseOrder()); // Max-heap
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder()); 
 
         long leftSum = 0;
         for (int i = 0; i < 2 * n; i++) {
-            leftPq.add(nums[i]);
+            maxHeap.add(nums[i]);
             leftSum += nums[i];
 
-            if (leftPq.size() > n) {
-                leftSum -= leftPq.poll();
+            if (maxHeap.size() > n) {
+                leftSum -= maxHeap.poll();
             }
 
-            if (i >= n - 1) {
-                leftMinSum[i] = leftSum;
-            }
+            leftMinSum[i] = leftSum;
         }
 
-        // ---------- Right Max Sum ----------
         long[] rightMaxSum = new long[N];
-        PriorityQueue<Integer> rightPq = new PriorityQueue<>(); // Min-heap
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(); // Min-heap
 
         long rightSum = 0;
         for (int i = N - 1; i >= n; i--) {
-            rightPq.add(nums[i]);
+            minHeap.add(nums[i]);
             rightSum += nums[i];
 
-            if (rightPq.size() > n) {
-                rightSum -= rightPq.poll();
+            if (minHeap.size() > n) {
+                rightSum -= minHeap.poll();
             }
-
-            if (i <= 2 * n) {
-                rightMaxSum[i] = rightSum;
-            }
+            
+            rightMaxSum[i] = rightSum;
         }
 
         // ---------- Find Minimum Difference ----------
         long ans = Long.MAX_VALUE;
+
         for (int i = n - 1; i < 2 * n; i++) {
             long diff = leftMinSum[i] - rightMaxSum[i + 1];
             ans = Math.min(ans, diff);
