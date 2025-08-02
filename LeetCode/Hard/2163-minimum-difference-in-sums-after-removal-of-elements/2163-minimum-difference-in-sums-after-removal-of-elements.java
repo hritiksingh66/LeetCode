a6022cd65fa2a3d931ad311218c87e4aad90a9f6
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-08-02 15:13:32 +0530

    2163-minimum-difference-in-sums-after-removal-of-elements Hard .java 08-02-2025 297 ms (9.03%)

--- LeetCode/Hard/2163-minimum-difference-in-sums-after-removal-of-elements/2163-minimum-difference-in-sums-after-removal-of-elements.java
+++ LeetCode/Hard/2163-minimum-difference-in-sums-after-removal-of-elements/2163-minimum-difference-in-sums-after-removal-of-elements.java
@@ -1,43 +1,55 @@
 class Solution {
- public long minimumDifference(int[] nums) {
-     int n = nums.length, k = n / 3;
-     long[] leftMins = new long[n]; 
-     long[] rightMaxs = new long[n];
-     long leftSum = 0, rightSum = 0, minDiff = Long.MAX_VALUE;
+    public long minimumDifference(int[] nums){
+        int n = nums.length;
+        int k = n/3;
 
-     PriorityQueue<Integer> maxLeftHeap = new PriorityQueue<>((a, b) -> b - a);
-     PriorityQueue<Integer> minRightHeap = new PriorityQueue<>();        
+        long[] leftMinSum = new long[n];
+        PriorityQueue<Integer> leftPq = new PriorityQueue<>((a,b) -> b-a);
+        
+        leftMinSum[0] = nums[0];
+        leftPq.add(nums[0]);
 
-     for (int i = 0; i < k; i++) {
-         maxLeftHeap.offer(nums[i]);
-         leftSum += nums[i];
-     }
-     leftMins[k - 1] = leftSum;
-     for (int i = k; i < n - k; i++) {
-         int x = nums[i];
-         if (x < maxLeftHeap.peek()) {
-             leftSum += x - maxLeftHeap.poll();
-             maxLeftHeap.offer(x);
-         }
-         leftMins[i] = leftSum;
-     }
-     for (int i = n - 1; i >= n - k; i--) {
-         minRightHeap.offer(nums[i]);
-         rightSum += nums[i];
-     }
-     rightMaxs[n - k] = rightSum;
+        for(int i = 1 ; i < k ; i++){
+            leftMinSum[i] = nums[i]+leftMinSum[i-1];
+            leftPq.add(nums[i]);
+        }
 
-     for (int i = n - k - 1; i >= k - 1; i--) {
-         int x = nums[i];
-         if (x > minRightHeap.peek()) {
-             rightSum += x - minRightHeap.poll();
-             minRightHeap.offer(x);
-         }
-         rightMaxs[i] = rightSum;
-     }
-     for (int i = k - 1; i < n - k; i++) {
-         minDiff = Math.min(minDiff, leftMins[i] - rightMaxs[i + 1]);
-     }
-     return minDiff;
- }
+        for(int i = k ; i < 2*k ; i++){
+            leftPq.add(nums[i]);
+            if(leftPq.size() > k){
+                leftMinSum[i] = leftMinSum[i-1] + nums[i] - leftPq.poll();
+            }
+        }
+
+        System.out.println(Arrays.toString(leftMinSum));
+
+        long[] rightMaxSum = new long[n];
+        PriorityQueue<Integer> rightPq = new PriorityQueue<>();
+
+        rightMaxSum[n-1] = nums[n-1];
+        rightPq.add(nums[n-1]);
+
+        for(int i = n-2 ; i >= n-k ; i--){
+            rightMaxSum[i] = nums[i] + rightMaxSum[i+1];
+            rightPq.add(nums[i]);
+        }
+
+        for(int i = n-k-1 ; i >= k ; i--){
+            rightPq.add(nums[i]);
+            if(rightPq.size() > k){
+                rightMaxSum[i] = rightMaxSum[i+1] + nums[i] - rightPq.poll();
+            }
+        }
+        System.out.println(Arrays.toString(rightMaxSum));
+
+        long ans = Long.MAX_VALUE;
+
+        for(int i = k-1 ; i < 2*k ; i++){
+            long diff = (leftMinSum[i] - rightMaxSum[i+1]);
+            ans = Math.min(diff,ans);
+        }
+
+        return ans;
+        
+    }
 }
