4defb670c5b72a536de2785776040a1b70c19ca1
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-08-03 11:23:03 +0530

    0130-surrounded-regions Medium .java 08-03-2025 1 ms (99.65%)

---
+++ LeetCode/Medium/0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,43 @@
+class Solution {
+    int m;
+    int n;
+
+    public void solve(char[][] board) {
+        m = board.length;
+        n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(board, 0, j);
+            dfs(board, m - 1, j);
+        }
+
+
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n ; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }else if(board[i][j] == 'T'){
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    void dfs(char[][] board, int i, int j) {
+        if (i < 0 || i >= m  || j < 0 || j >= n || board[i][j] != 'O') {
+            return;
+        }
+
+        board[i][j] = 'T';
+
+        dfs(board, i + 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i - 1, j);
+        dfs(board, i, j - 1);
+    }
+}
