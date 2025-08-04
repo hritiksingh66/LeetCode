6282e74b83bef8a811a6b7f8c54b63b32bea969d
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-08-05 01:35:48 +0530

    0008-string-to-integer-atoi Medium .java 08-05-2025 1 ms (100%)

--- LeetCode/Medium/0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ LeetCode/Medium/0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,36 +1,7 @@
-// class Solution {
-//     public int myAtoi(String s) {
-//         int n = s.length();
-//         int i = 0;
-
-//         while( i < n && s.charAt(i) == ' '){ // skip leading white spaces
-//             i++;
-//         }
-
-//         int sign = 1;
-
-//         while(i < n && (s.charAt(i)=='+' || s.charAt(i)=='-')){
-//            sign = s.charAt(i)=='+' ? 1 : -1;
-//            i++;
-//         }
-
-//         int result = 0; 
-
-//         while (i < n && Character.isDigit(s.charAt(i))){
-//             result = result * 10 + (s.charAt(i) - '0');
-//             if(sign * result >= Integer.MAX_VALUE) return Integer.MAX_VALUE;
-//             if(sign * result <= Integer.MIN_VALUE) return Integer.MIN_VALUE;
-//             i++;
-//         }
-
-//         return (int)(sign * result);
-//     }
-// }
 class Solution {
     public int myAtoi(String s) {
         int i = 0, n = s.length();
 
-        // 1. Skip leading whitespaces
         while (i < n && s.charAt(i) == ' ') {
             i++;
         }
@@ -46,11 +17,14 @@ class Solution {
 
         while (i < n && Character.isDigit(s.charAt(i))) {
             result = result * 10 + (s.charAt(i) - '0');
-            if (sign * result >= Integer.MAX_VALUE) return Integer.MAX_VALUE;
-            if (sign * result <= Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            if (sign * result >= Integer.MAX_VALUE)
+                return Integer.MAX_VALUE;
+            if (sign * result <= Integer.MIN_VALUE)
+                return Integer.MIN_VALUE;
             i++;
         }
 
         return (int)(sign * result);
     }
 }
+
