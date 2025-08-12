0552cb151e4825eeeea898a7c0d564b9dfc866be
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-08-13 00:53:09 +0530

    2787-ways-to-express-an-integer-as-sum-of-powers Medium .java 08-13-2025 13 ms (83.68%)

--- LeetCode/Medium/2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.java
+++ LeetCode/Medium/2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.java
@@ -1,41 +1,28 @@
 class Solution {
-    int[][] dp;
-    static final int M = 1_000_000_007;
+    static final int MOD = 1_000_000_007;
 
     public int numberOfWays(int n, int x) {
         int target = n;
 
-        n = 0;
-
-        while (Math.pow(n + 1, x) <= target) {
+        n = 1;
+        while (Math.pow(n, x) <= target) {
             n++;
         }
-        int[] arr = new int[n];
 
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = (int) Math.pow(i + 1, x);
         }
 
-        dp = new int[n + 1][target + 1];
+        int[] dp = new int[target + 1];
+        dp[0] = 1; 
 
-        for (int i = 0; i <= n ; i++) {
-            dp[i][0] = 1;
-        }
-
-        return solve(arr, target, n);
-    }
-
-    private int solve(int[] arr, int target, int n) {
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < target + 1; j++) {
-                if (arr[i - 1] <= j) {
-                    dp[i][j] = (int)(((long)dp[i - 1][j - arr[i - 1]] + dp[i - 1][j])%M);
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
+        for (int num : arr) {
+            for (int sum = target; sum >= num; sum--) {
+                dp[sum] = (dp[sum] + dp[sum - num]) % MOD;
             }
         }
 
-        return dp[n][target];
+        return dp[target];
     }
 }
