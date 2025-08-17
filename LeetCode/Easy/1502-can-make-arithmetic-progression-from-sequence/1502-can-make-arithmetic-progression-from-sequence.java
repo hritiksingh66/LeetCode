e460828a4e778a73a7ca55e16a8486600d8abe14
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-08-18 00:32:15 +0530

    1502-can-make-arithmetic-progression-from-sequence Easy .java 08-18-2025 1 ms (99.18%)

--- LeetCode/Easy/1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ LeetCode/Easy/1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -1,33 +1,33 @@
 class Solution {
-    public boolean canMakeArithmeticProgression(int[] arr){
+    public boolean canMakeArithmeticProgression(int[] arr) {
         int n = arr.length;
-        Set<Integer> set = new HashSet<>();
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
 
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-
-        for(int num : arr){
-            min = Math.min(num,min);
-            max = Math.max(num,max);
-            set.add(num);
+        for (int num : arr) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
         }
 
         int diff = max - min;
-        if(diff % (n-1) != 0){
-            return false;
-        }
+        if (diff % (n - 1) != 0) return false;
 
-        int cd = diff / (n-1);
+        int cd = diff / (n - 1);
+        if (cd == 0) return true;
 
         int i = 0;
-        while(i < n){
-            int num = min + i * cd;
-            if(!set.contains(num)){
-                return false;
-            }
-            i++;
-        }
+        while (i < n) {
+            if (arr[i] == min + i * cd) {
+                i++;
+            } else {
+                int k = (arr[i] - min) / cd;
 
+                if ((arr[i] - min) % cd != 0 || arr[i] == arr[k]) return false;
+
+                int temp = arr[i];
+                arr[i] = arr[k];
+                arr[k] = temp;
+            }
+        }
         return true;
     }
 }
