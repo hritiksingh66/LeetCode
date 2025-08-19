51bffacd812dce62256bbfe770130f30c9f32dcd
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-08-20 00:59:03 +0530

    2348-number-of-zero-filled-subarrays Medium .java 08-20-2025 175 ms (4.39%)

---
+++ LeetCode/Medium/2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        int currSubLen = 0;
+        long res = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                currSubLen++;
+            } else {
+                if (currSubLen > 0) {
+                    System.out.println(currSubLen);
+                    res += (long)currSubLen * (currSubLen + 1)/2;
+                    currSubLen = 0;
+                }
+            }
+        }
+
+        if(currSubLen > 0){
+            System.out.println(currSubLen);
+            res += (long)currSubLen * (currSubLen + 1)/2;
+        }
+
+        return res;
+
+    }
+}
