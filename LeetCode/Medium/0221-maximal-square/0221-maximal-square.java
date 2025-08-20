5ec36743652d38fca48295f763e511e82d61f683
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-08-20 20:52:12 +0530

    0221-maximal-square Medium .java 08-20-2025 12 ms (16.04%)

--- LeetCode/Medium/0221-maximal-square/0221-maximal-square.java
+++ LeetCode/Medium/0221-maximal-square/0221-maximal-square.java
@@ -1,29 +1,30 @@
+
+
 class Solution {
-    int m , n;
+    int m, n;
     int[][] dp;
-    public int maximalSquare(char[][] matrix){
+
+    public int maximalSquare(char[][] matrix) {
         m = matrix.length;
         n = matrix[0].length;
 
-        dp = new int[m+1][n+1];
+        dp = new int[m + 1][n + 1];
 
-        for(int i = 0 ; i <= m ; i++){
-            Arrays.fill(dp[i],-1);
+        for (int i = 0; i <= m; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        
-        int largestSquare = Integer.MIN_VALUE;
+
+        int res = Integer.MIN_VALUE;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == '1') {
-                    largestSquare = Math.max(largestSquare,solve(i, j, matrix));
+                    res = Math.max(res,solve(i, j, matrix));
                 }
             }
         }
 
-        int area = largestSquare * largestSquare;
-
-        return area;
+        return res*res;
     }
 
     private int solve(int i, int j, char[][] matrix) {
@@ -31,7 +32,7 @@ class Solution {
             return 0;
         }
 
-        if(dp[i][i] != -1){
+        if (dp[i][j] != -1) {
             return dp[i][j];
         }
 
@@ -41,5 +42,4 @@ class Solution {
 
         return dp[i][j] = 1 + Math.min(right, Math.min(diag, down));
     }
-
 }
