e9e6c8637dc0fdbbc881e36bdbb57a40cc78a1b9
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-08-20 20:29:15 +0530

    1277-count-square-submatrices-with-all-ones Medium .java 08-20-2025 6 ms (75%)

--- LeetCode/Medium/1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ LeetCode/Medium/1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -6,25 +6,20 @@ class Solution{
         int n = matrix[0].length;
 
         int[][] t = new int[m][n]; 
+        int res = 0;
 
         for(int i = 0 ; i < m ; i++){
             for(int j = 0; j < n ; j++){
                 if(i == 0 || j == 0){
                     t[i][j] = matrix[i][j];
-                    continue;
+                    res += t[i][j];
+                }else{
+                    if(matrix[i][j] == 1){
+                        t[i][j] = 1 + Math.min(t[i-1][j] , Math.min(t[i][j-1],t[i-1][j-1]));
+                        res += t[i][j];
+                    }
                 }
 
-                if(matrix[i][j] == 1){
-                    t[i][j] = 1 + Math.min(t[i-1][j] , Math.min(t[i][j-1],t[i-1][j-1]));
-                }
-            }
-        }
-
-        int res = 0;
-        
-        for(int i = 0 ; i < m; i++){
-            for(int j = 0 ; j < n ; j++){
-                res += t[i][j];
             }
         }
 
