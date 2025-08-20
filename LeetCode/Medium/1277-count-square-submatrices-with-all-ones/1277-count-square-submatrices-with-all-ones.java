c26160a0591c2fda06ffb744cd30c8884a7014b2
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-08-20 20:21:15 +0530

    1277-count-square-submatrices-with-all-ones Medium .java 08-20-2025 6 ms (75%)

--- LeetCode/Medium/1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ LeetCode/Medium/1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,43 +1,33 @@
-class Solution {
-    int m, n;
-    int[][] dp;
-
+//Bottom Up Approach
+class Solution{
     public int countSquares(int[][] matrix) {
-        m = matrix.length;
-        n = matrix[0].length;
 
-        dp = new int[m + 1][n + 1];
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        for (int i = 0; i <= m; i++) {
-            Arrays.fill(dp[i], -1);
-        }
+        int[][] t = new int[m][n]; 
 
-        int res = 0;
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0; j < n ; j++){
+                if(i == 0 || j == 0){
+                    t[i][j] = matrix[i][j];
+                    continue;
+                }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 1) {
-                    res += solve(i, j, matrix);
+                if(matrix[i][j] == 1){
+                    t[i][j] = 1 + Math.min(t[i-1][j] , Math.min(t[i][j-1],t[i-1][j-1]));
                 }
             }
         }
 
+        int res = 0;
+        
+        for(int i = 0 ; i < m; i++){
+            for(int j = 0 ; j < n ; j++){
+                res += t[i][j];
+            }
+        }
+
         return res;
-    }
-
-    private int solve(int i, int j, int[][] matrix) {
-        if (i >= m || j >= n || matrix[i][j] == 0) {
-            return 0;
-        }
-
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int right = solve(i, j + 1, matrix);
-        int diag = solve(i + 1, j + 1, matrix);
-        int down = solve(i + 1, j, matrix);
-
-        return dp[i][j] = 1 + Math.min(right, Math.min(diag, down));
     }
 }
