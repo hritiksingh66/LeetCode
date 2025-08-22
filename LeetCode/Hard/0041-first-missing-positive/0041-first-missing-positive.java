c95570199a97f5cd058113600bfc70dd43cf86e6
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-08-22 12:19:01 +0530

    0041-first-missing-positive Hard .java 08-22-2025 1 ms (100%)

--- LeetCode/Hard/0041-first-missing-positive/0041-first-missing-positive.java
+++ LeetCode/Hard/0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,22 +1,26 @@
 class Solution {
-    public int firstMissingPositive(int[] nums){
+    public int firstMissingPositive(int[] nums) {
         int n = nums.length;
 
-        Set<Integer> set = new HashSet<>();
-
-        for(int num : nums){
-            set.add(num);
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                int crktIdx = nums[i] - 1;
+                int temp = nums[i];
+                nums[i] = nums[crktIdx];
+                nums[crktIdx] = temp;
+            }
         }
 
-        for(int i = 1 ; i <= n ; i++){
+        int i = 1;
 
-            if(!set.contains(i)){
+        for (int num : nums) {
+            if (num != i) {
                 return i;
             }
-
+            i++;
         }
 
-        return n+1;
-        
+        return n + 1;
+
     }
 }
