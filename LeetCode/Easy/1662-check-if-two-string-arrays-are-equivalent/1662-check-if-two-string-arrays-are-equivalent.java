eef5b5ee32c3c42ce06ebce3a1c4fd37abf17044
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-08-25 16:10:03 +0530

    1662-check-if-two-string-arrays-are-equivalent Easy .java 08-25-2025 1 ms (61.97%)

---
+++ LeetCode/Easy/1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2){
+        String str1 = "";
+
+        for(String word : word1){
+            str1 += word;
+        }
+
+        String str2 = "";
+
+        for(String word : word2){
+            str2 += word;
+        }
+        
+
+        return str1.equals(str2);
+    }
+}
