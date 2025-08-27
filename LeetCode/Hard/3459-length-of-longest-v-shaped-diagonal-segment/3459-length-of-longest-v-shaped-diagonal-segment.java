32e3a9b94aeb2bd81c08e83ca662e562995ee05f
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-08-27 14:00:19 +0530

    3459-length-of-longest-v-shaped-diagonal-segment Hard .java 08-27-2025 479 ms (84.21%)

--- LeetCode/Hard/3459-length-of-longest-v-shaped-diagonal-segment/3459-length-of-longest-v-shaped-diagonal-segment.java
+++ LeetCode/Hard/3459-length-of-longest-v-shaped-diagonal-segment/3459-length-of-longest-v-shaped-diagonal-segment.java
@@ -1,9 +1,12 @@
 class Solution {
     private int[][] dir = {{1,1},{1,-1},{-1,-1},{-1,1}};
-    private int n , m; 
+    private int n, m;
+    private int[][][][] dp;   // ✅ declare only, don’t initialize here
+
     public int lenOfVDiagonal(int[][] grid) {
         n = grid.length;
         m = grid[0].length;
+        dp = new int[n][m][4][2];   // ✅ initialize after knowing n & m
 
         int maxLength = 0;
 
@@ -11,40 +14,42 @@ class Solution {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
                     for (int d = 0; d < 4; d++) {
-                        maxLength = Math.max(maxLength, 1 + dfs(grid, i, j, 2, d,true));
+                        maxLength = Math.max(maxLength, 1 + dfs(grid, i, j, 2, d, true));
                     }
                 }
             }
         }
 
         return maxLength;
-
     }
 
-    private boolean isValid(int[][] grid, int i, int j, int expected){
-        return i >= 0 && i < n && j >=0 && j < m && grid[i][j] == expected; 
+    private boolean isValid(int[][] grid, int i, int j, int expected) {
+        return i >= 0 && i < n && j >= 0 && j < m && grid[i][j] == expected; 
     }
 
-    private int dfs(int[][] grid, int i, int j, int expected, int d , boolean canTurn) {
-
+    private int dfs(int[][] grid, int i, int j, int expected, int d, boolean canTurn) {
         int newI = i + dir[d][0];
         int newJ = j + dir[d][1];
 
-
-        if(!isValid(grid,newI,newJ,expected)){
+        if (!isValid(grid, newI, newJ, expected)) {
             return 0;
         }
 
-        int newExp = (expected == 2 )? 0 : 2;
+        int cIdx = canTurn ? 1 : 0;
 
-
-        int maxLength = dfs(grid, newI, newJ, newExp, d ,canTurn);
-
-        if(canTurn){
-            int newDir = (d+1)%4;
-            maxLength = Math.max(maxLength , dfs(grid,newI,newJ,newExp,newDir,false));
+        if (dp[newI][newJ][d][cIdx] != 0) {
+            return dp[newI][newJ][d][cIdx];
         }
 
-        return maxLength + 1;
+        // int newExp = (expected == 2) ? 0 : 2; 
+
+        int maxLength = dfs(grid, newI, newJ, 2 - expected, d, canTurn);
+
+        if (canTurn) {
+            int newDir = (d + 1) % 4;
+            maxLength = Math.max(maxLength, dfs(grid, newI, newJ, 2 - expected, newDir, false));
+        }
+
+        return dp[newI][newJ][d][cIdx] = maxLength + 1;
     }
 }
