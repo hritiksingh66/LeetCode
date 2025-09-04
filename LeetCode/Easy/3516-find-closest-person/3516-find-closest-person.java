dc2d8a75b1bb78d9dafb11ff2f7f3e8b755ddd87
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-09-04 23:50:28 +0530

    3516-find-closest-person Easy .java 09-04-2025 0 ms (100%)

---
+++ LeetCode/Easy/3516-find-closest-person/3516-find-closest-person.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findClosest(int x, int y, int z){
+
+        x = Math.abs(x-z);
+        y = Math.abs(y-z);
+
+        if(x<y){
+            return 1;
+        }else if(y < x){
+            return 2;
+        }
+
+        return 0;
+        
+    }
+}
