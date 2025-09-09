8a8cdde264f947807e80f09c597d25333e54064d
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-09-09 17:28:18 +0530

    2327-number-of-people-aware-of-a-secret Medium .java 09-09-2025 18 ms (16.36%)

--- LeetCode/Medium/2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.java
+++ LeetCode/Medium/2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.java
@@ -1,41 +1,29 @@
 class Solution {
     int M = 1000000007;
-    int f;
-    int del;
+    public int peopleAwareOfSecret(int n, int delay, int forget) {
+        int[] dp = new int[n + 1];
 
-    public int peopleAwareOfSecret(int n, int delay, int forget){
-        int total = 0;
-        f = forget;
-        del = delay;
-        int[] dp = new int[n+1];
         Arrays.fill(dp , -1);
 
-        for(int day = n-forget+1 ; day <= n ; day++){
-            if(day > 0){
-                total = (total + solve(day,dp)) % M; 
+        dp[1] = 1; // we already know that till day 1 only one person knew the secret'
+
+        for (int day = 2; day <= n; day++) {
+            int count = 0;
+            for (int prev = day - forget + 1; prev <= day - delay; prev++) {
+                if(prev > 0){
+                    count = (count + dp[prev]) % M;
+                }
             }
+            dp[day] = count;
         }
 
-        return total;
-    }
-
-    public int solve(int day,int[] dp){
-        if(day == 1){
-            return 1;
-        }
-
-        if(dp[day] != -1){
-            return dp[day];
-        }
 
         int res = 0;
 
-        for(int d = day-f+1 ; d <= day-del ; d++){
-            if(d > 0){
-                res = (res + solve(d,dp))%M;
-            }
+        for(int day = n - forget + 1 ; day <= n ; day++){
+            res = (res + dp[day])%M;
         }
 
-        return dp[day] = res;
+        return res;
     }
 }
