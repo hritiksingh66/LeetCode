1ce97d1b1cd41c52e6fbdf8107128fa69f589faf
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-09-24 14:09:41 +0530

    0166-fraction-to-recurring-decimal Medium .java 09-24-2025 1 ms (99.8%)

---
+++ LeetCode/Medium/0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String fractionToDecimal(int num, int den) {
+        if (num == 0)
+            return "0";
+
+        StringBuilder sb = new StringBuilder();
+
+        if ((num < 0) ^ (den < 0)) {
+            sb.append("-");
+        }
+
+        long absNum = Math.abs((long) num);
+        long absDen = Math.abs((long) den);
+
+        long intDiv = absNum / absDen;
+        sb.append(intDiv);
+
+        long rem = absNum % absDen;
+
+        if (rem == 0) {
+            return sb.toString();
+        }
+
+        sb.append(".");
+
+        Map<Long, Integer> mp = new HashMap<>();
+
+        while (rem != 0) {
+
+            if (mp.containsKey(rem)) {
+                int pos = mp.get(rem);
+                sb.insert(pos, "(");
+                sb.append(")");
+                break;
+            }
+
+            mp.put(rem, sb.length());
+
+            rem *= 10;
+
+            long digit = rem / absDen;
+            sb.append(digit);
+            rem %= absDen;
+        }
+
+        return sb.toString();
+    }
+}
