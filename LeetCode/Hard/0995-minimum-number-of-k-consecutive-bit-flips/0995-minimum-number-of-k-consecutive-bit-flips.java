d2d60d6bf249e67292c208b639e8301a5d9c13fd
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-09-28 11:23:09 +0530

    0995-minimum-number-of-k-consecutive-bit-flips Hard .java 09-28-2025 17 ms (5.26%)

--- LeetCode/Hard/0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.java
+++ LeetCode/Hard/0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.java
@@ -1,32 +1,28 @@
-// Approach - 1
-// TC = O(n)  as we are iterating only one for loop n times
-// SC = O(n)  as we declared a new boolean of size n , means using extra space 
-class Solution{
+class Solution {
     public int minKBitFlips(int[] nums, int k){
-        
         int n = nums.length;
-        
+
         int flips = 0;
-        
-        int flipCountFromPastFori = 0;
-        
-        boolean[] isFlipped = new boolean[n];
-        Arrays.fill(isFlipped,false);
-        
-        for(int i = 0; i < n; i++){
-            if(i>=k && isFlipped[i-k] == true){
-                flipCountFromPastFori--;           
+
+        int flipCount = 0;
+
+        Set<Integer> isFlipped = new HashSet<>();
+
+        for(int i = 0 ; i < n ; i++){
+
+            if(i >= k && isFlipped.contains(i-k)){
+                flipCount--;
             }
-            
-            if(flipCountFromPastFori % 2 == nums[i]){ //flip at index i
-                if(i + k > n){
-                    return -1;
-                }
-                flipCountFromPastFori++;
-                flips++;
-                isFlipped[i] = true;
+
+            if(flipCount % 2 == nums[i]){ // flip
+                if(i+k > n) return -1;
+                flipCount++;
+                flips += 1;
+                isFlipped.add(i);
             }
+
         }
+
         return flips;
     }
 }
