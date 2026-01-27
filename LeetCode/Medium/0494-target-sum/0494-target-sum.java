9522628b53a804d4a0acfbd14986ff944636ec1b
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2026-01-27 16:10:37 +0530

    0494-target-sum Medium .java 01-27-2026 N/A (0%)

--- LeetCode/Medium/0494-target-sum/0494-target-sum.java
+++ LeetCode/Medium/0494-target-sum/0494-target-sum.java
@@ -1,37 +1,51 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target){
+    static int[][] dp;
+
+    public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
 
         int sum = 0;
 
-        for(int num : nums){
+        for (int num : nums) {
             sum += num;
         }
 
-        sum = sum + target;
-
-        if(sum%2 != 0){
+        if (Math.abs(target) > sum) {
             return 0;
         }
 
-        sum = sum/2;
 
-        return solve(nums,sum,n);
+        sum = (sum + target)/2;
+        
+        if (sum % 2 != 0) {
+            return 0;
+        }
+
+        dp = new int[n + 1][sum + 1];
+
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        return solve(nums, sum, n);
     }
 
-    int solve(int[] nums, int sum , int n){
-        if(sum == 0 && n==0){
-            return 1;
+    int solve(int[] nums, int sum, int n) {
+        if (n == 0) {
+            return sum == 0 ? 1 : 0;
         }
 
-        if(n == 0){
-            return 0;
+        if (dp[n][sum] != -1) {
+            return dp[n][sum];
         }
 
-        if(nums[n-1] <= sum){
-            return solve(nums,sum-nums[n-1],n-1) + solve(nums,sum,n-1);
+        if (nums[n - 1] <= sum) {
+            dp[n][sum] = solve(nums, sum-nums[n-1],n-1) + solve(nums,sum,n-1);
+        }else{
+            dp[n][sum] = solve(nums, sum, n - 1);
         }
 
-        return solve(nums,sum,n-1);
+        return dp[n][sum];
+
     }
 }
