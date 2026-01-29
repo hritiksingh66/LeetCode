60aec3689dc84b34cd574e3f4208619e59bcffc5
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2026-01-30 01:00:58 +0530

    0307-range-sum-query-mutable Medium .java 01-30-2026 N/A (0%)

---
+++ LeetCode/Medium/0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -0,0 +1,71 @@
+class NumArray {
+    static int[] segTree;
+    static int n;
+
+    public NumArray(int[] nums){
+        n = nums.length;
+
+        segTree = new int[2*n-1];
+
+        buildTree(nums,0,0,n-1);
+    }
+
+    void buildTree(int[] nums, int i, int l , int r){
+        // Base Case
+        if(l==r){
+            segTree[i] = nums[l];
+            return;
+        }
+
+        int mid = (l+r)/2;
+
+        buildTree(nums,2*i+1,l,mid);
+        buildTree(nums,2*i+2,mid+1,r);
+
+        segTree[i] = segTree[2*i+1] + segTree[2*i+2];
+    }
+    
+    public void update(int index, int val){
+        updte(index,val, 0, 0, n-1);
+    }
+
+    public void updte(int idx , int val, int i, int l, int r){
+        if(l==r){
+            segTree[i] = val;
+            return;
+        }
+
+        int mid = (l+r)/2;
+
+        if(idx <= mid){
+            updte(idx,val,2*i+1,l,mid);
+        }else{
+            updte(idx,val,2*i+2,mid+1,r);
+        }
+
+        segTree[i] = segTree[2*i+1] + segTree[2*i+2];
+    }
+    
+    public int sumRange(int left, int right){
+        return sum(left,right,0,0,n-1);
+    }
+
+    public int sum( int st, int ed, int i, int l, int r){
+        // Complete Out Of Bound Case
+        if(l > ed || r < st){
+            return 0;
+        }else if( st <= l && r <= ed){ // entirely under range
+            return segTree[i];
+        }else{ // OverLapping Case
+            int mid = (l+r)/2;
+            return sum(st,ed, 2*i+1,l,mid) + sum(st,ed,2*i+2,mid+1,r);
+        }
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(index,val);
+ * int param_2 = obj.sumRange(left,right);
+ */
